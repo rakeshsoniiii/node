c0e0c6fb86f0373037fe8e3c3705af1cf99c6307
Rakesh Soni <149389286+rakeshsoniiii@users.noreply.github.com>
2024-09-17 17:55:45 +0530

    node update2 .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,6 @@ const path = require('path');
 
 const NodePlugin = require('./tools/node_modules/eslint-plugin-node-core');
 NodePlugin.RULES_DIR = path.resolve(__dirname, 'tools', 'eslint-rules');
-
-// The Module._findPath() monkeypatching is to make it so that ESLint will work
-// if invoked by a globally-installed ESLint or ESLint installed elsewhere
-// rather than the one we ship. This makes it possible for IDEs to lint files
-// with our rules while people edit them.
 const ModuleFindPath = Module._findPath;
 const hacks = [
   'eslint-plugin-node-core',
